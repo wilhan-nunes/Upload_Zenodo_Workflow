bd18d7a663dd85c8dff8fd530b31550c97e2e3ca
Wilhan Nunes <85765131+wilhan-nunes@users.noreply.github.com>
2025-02-20 15:21:46 -0800

    introducing functions

--- bin/process_upload_zenodo.py
+++ bin/process_upload_zenodo.py
@@ -1,10 +1,15 @@
-import requests
 import argparse
-from dotenv import dotenv_values
 import json
-import yaml
-import os
 import logging
+import os
+
+import requests
+import yaml
+from dotenv import dotenv_values
+
+# SERVER_URL = 'https://zenodo.org/api/deposit/depositions'
+SERVER_URL = 'https://sandbox.zenodo.org/api/deposit/depositions'  # SANDBOX
+
 
 def setup_logging(log_file):
     logging.basicConfig(
@@ -15,30 +20,87 @@ def setup_logging(log_file):
     )
 
 
+def create_empty_upload(access_token):
+    headers = {"Content-Type": "application/json"}
+    params = {'access_token': access_token}
+    url = SERVER_URL
+    try:
+        r = requests.post(url, params=params, json={}, headers=headers)
+        r.raise_for_status()
+        new_deposition_json = r.json()
+        deposition_id = new_deposition_json['id']
+        bucket_url = new_deposition_json["links"]["bucket"]
+        logging.info(f"Empty deposition created successfully with ID: {deposition_id}")
+        return deposition_id, bucket_url
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error creating empty deposition: {e}")
+        raise
+
+
+def upload_file_to_zenodo(bucket_url, path, access_token):
+    params = {'access_token': access_token}
+    with open(path, "rb") as fp:
+        r = requests.put(f'{bucket_url}/{path}', data=fp, params=params)
+        if r.status_code == 201:
+            logging.info(f"File uploaded successfully.")
+        else:
+            logging.error(f"Error uploading file. Status code: {r.status_code}")
+        r.raise_for_status()
+
+
+def send_metadata_to_server(deposition_id, data, access_token):
+    headers = {"Content-Type": "application/json"}
+    r = requests.put(f'{SERVER_URL}/{deposition_id}',
+                     params={'access_token': access_token},
+                     data=json.dumps(data),
+                     headers=headers)
+    if r.status_code == 200:
+        logging.info(f"Metadata sent successfully to the server.")
+    else:
+        logging.error(f"Error sending metadata to the server. Status code: {r.status_code}")
+    r.raise_for_status()
+
+
 def delete_deposited_files(deposition_id, params):
     # Delete files already deposited
-    files_url = f'https://sandbox.zenodo.org/api/deposit/depositions/{deposition_id}/files'
+    files_url = f'{SERVER_URL}/{deposition_id}/files'
     files_response = requests.get(files_url, params=params)
     files_response.raise_for_status()
     files = files_response.json()
     for file in files:
-        delete_url = f'https://sandbox.zenodo.org/api/deposit/depositions/{deposition_id}/files/{file["id"]}'
+        delete_url = f'{SERVER_URL}/{deposition_id}/files/{file["id"]}'
         delete_response = requests.delete(delete_url, params=params)
         delete_response.raise_for_status()
 
 
 def create_new_version(deposition_id, access_token):
-    url = f'https://sandbox.zenodo.org/api/deposit/depositions/{deposition_id}/actions/newversion'
+    url = f'{SERVER_URL}/{deposition_id}/actions/newversion'
     headers = {"Content-Type": "application/json"}
     params = {'access_token': access_token}
-    response = requests.post(url, params=params, headers=headers)
-    response.raise_for_status()
-    new_version = response.json()
-    new_deposition_id = new_version['links']['latest_draft'].split('/')[-1]
+    logging.info(f"Creating new version for deposition ID: {deposition_id}")
+    try:
+        response = requests.post(url, params=params, headers=headers)
+        response.raise_for_status()
+        new_version = response.json()
+        new_deposition_id = new_version['links']['latest_draft'].split('/')[-1]
+        logging.info(f"Zenodo deposition ID (new version): {new_deposition_id}")
 
-    delete_deposited_files(new_deposition_id, params)
+        delete_deposited_files(new_deposition_id, params)
 
-    return new_deposition_id, new_version['links']['bucket']
+        return new_deposition_id, new_version['links']['bucket']
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error creating new version: {e}")
+        raise
+
+
+def publish_deposition(deposition_id, access_token):
+    r = requests.post(f'{SERVER_URL}/{deposition_id}/actions/publish',
+                      params={'access_token': access_token})
+    if r.status_code == 202:
+        logging.info(f"Deposition published successfully.")
+    else:
+        logging.error(f"Error publishing deposition. Status code: {r.status_code}")
+    r.raise_for_status()
 
 
 def main():
@@ -52,10 +114,10 @@ def main():
     setup_logging(args.output_deposition_log)
 
     parameters_json = yaml.safe_load(open(args.input_yaml_params))
-    # SERVER_URL = 'https://zenodo.org/api/deposit/depositions'
-    SERVER_URL = 'https://sandbox.zenodo.org/api/deposit/depositions' #SANDBOX
     config = dotenv_values()
+    ACCESS_TOKEN = config['TOKEN']
 
+    # Extracting metadata from the parameters
     creators_list = [{'name': name.strip(), 'affiliation': affiliation.strip()}
                      for name, affiliation in zip(parameters_json['metadata.creators'].split(';')[::2],
                                                   parameters_json['metadata.creators'].split(';')[1::2])]
@@ -70,6 +132,11 @@ def main():
     elif access_right == "open" and embargo_date:
         raise ValueError("The 'embargo_date' must be empty when 'access_right' is set to 'open'.")
 
+    gnps2_task_url = f"https://gnps2.org/status?task={parameters_json["uploaded_task_id"]}"
+    deposition_notes = f'{parameters_json["metadata.notes"]}; <a href="{gnps2_task_url}">GNPS2 Task</a>' \
+        if parameters_json[
+               "metadata.notes"] != '' else f'<a href="{gnps2_task_url}">GNPS2 Task</a>'
+
     data = {
         "metadata": {
             "title": parameters_json["metadata.title"],
@@ -81,7 +148,11 @@ def main():
             "access_right": access_right,
             "license": parameters_json["metadata.license"],
             "embargo_date": embargo_date,
-            "notes": parameters_json["metadata.notes"]
+            "notes": deposition_notes,
+            "related_identifiers": [
+                {"identifier": gnps2_task_url,
+                 "relation": "isSupplementTo",
+                 "resource_type": "dataset"}]
         }
     }
 
@@ -90,63 +161,27 @@ def main():
     path = args.input_upload_file
 
     if not dry_run:
-        # Create a new empty upload
-        ACCESS_TOKEN = config['TOKEN']
-        headers = {"Content-Type": "application/json"}
-        params = {'access_token': ACCESS_TOKEN}
         if new_version:
+            # Create a new version of the deposition
             deposition_id, bucket_url = create_new_version(parameters_json['zenodo_deposition_id'], ACCESS_TOKEN)
-            logging.info(f"Zenodo deposition ID (new version): {deposition_id}")
         else:
-            r = requests.post(SERVER_URL, params=params, json={}, headers=headers)
-            if r.status_code == 201:
-                logging.info(f"Empty deposition created successfully.")
-            else:
-                logging.error(f"Error creating empty deposition. Status code: {r.status_code}")
-            r.raise_for_status()
-
-            new_deposition_json = r.json()
-            deposition_id = new_deposition_json['id']
-            bucket_url = new_deposition_json["links"]["bucket"]
-            logging.info(f"Zenodo deposition ID: {deposition_id}")
+            # Create a new empty upload
+            deposition_id, bucket_url = create_empty_upload(ACCESS_TOKEN)
 
         # Uploading the file
-        with open(path, "rb") as fp:
-            r = requests.put(f'{bucket_url}/{path}', data=fp, params=params)
-            if r.status_code == 201:
-                logging.info(f"File uploaded successfully.")
-            else:
-                logging.error(f"Error uploading file. Status code: {r.status_code}")
-            r.raise_for_status()
+        upload_file_to_zenodo(bucket_url, path, ACCESS_TOKEN)
 
         # Sending METADATA to the server
-        r = requests.put(f'{SERVER_URL}/{deposition_id}',
-                         params={'access_token': ACCESS_TOKEN},
-                         data=json.dumps(data),
-                         headers=headers)
-        if r.status_code == 200:
-            logging.info(f"Metadata sent successfully to the server.")
-        else:
-            logging.error(f"Error sending metadata to the server. Status code: {r.status_code}")
-        r.raise_for_status()
+        send_metadata_to_server(deposition_id, data, ACCESS_TOKEN)
 
         # Publish the deposition
-        r = requests.post(f'{SERVER_URL}/{deposition_id}/actions/publish',
-                          params={'access_token': ACCESS_TOKEN})
-        if r.status_code == 202:
-            logging.info(f"Deposition published successfully.")
-        else:
-            logging.error(f"Error publishing deposition. Status code: {r.status_code}")
-        r.raise_for_status()
-
-        # logging.info(f"Server response code: {r.status_code}")
-        # logging.info(f"Server response: {json.dumps(r.json(), indent=4)}")
+        publish_deposition(deposition_id, ACCESS_TOKEN)
 
     if dry_run:
         logging.info("### This was just a dry run test. No data was deposited ###")
 
     size_bytes = os.path.getsize(path)
-    file_size = f"{size_bytes / 1024 ** 2:.2f} MB." if size_bytes < 1024**3 else f"{size_bytes / 1024**3:.2f} GB"
+    file_size = f"{size_bytes / 1024 ** 2:.2f} MB." if size_bytes < 1024 ** 3 else f"{size_bytes / 1024 ** 3:.2f} GB"
     logging.info(f"Deposited file: {path}")
     logging.info(f"Deposited file size: {file_size}")
     logging.info(f"File generated from task ID: {parameters_json['uploaded_task_id']}")
